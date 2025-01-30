cd02903138c681091947072ecfeb09ba57f9757d
amir047123 <amirfaysal047123@gmail.com>
2025-01-30 23:03:40 +0600

    done

---
+++ comment.js
@@ -0,0 +1,29 @@
+// Javascripts Comments
+
+// Single Line Comments
+
+//  console log
+
+// console.log("Hello World");
+
+// let x = 10; // Variable Declaration
+// let y = 20; // Variable Declaration
+
+// Multi Line Comments
+
+/*
+
+let a = 18;
+
+
+let b = 25;
+let c = a + b;
+
+console.log(c);
+
+
+
+console.log("Hello World");
+
+
+*/
---
+++ variables.js
@@ -0,0 +1,109 @@
+// Javascripts variables
+
+// var
+// let
+// const
+
+// var name = " amir ";
+
+// console.log(name);
+
+// var name = "karim";
+// console.log(name);
+
+// let age = 27;
+
+// console.log(age);
+
+// age = 239;
+
+// console.log(age);
+
+// const country = "Bangladesh";
+
+// console.log(country);
+
+// Variables Naming Rules
+
+// a - z, A - Z, 0 - 9, _, $;
+
+// let const var fuction
+
+// let firstName = "amir";
+// console.log(firstName);
+
+// const $price = 100;
+
+// let _discount = 10;
+
+// Data Types
+
+// String  = " Amir"
+// Numbers = 12
+
+// Boolean= true or false
+// undefined = let x
+// Null = let x = null
+// Object = {name: "amir", age: 27}
+
+// let text = "Amir"; // string
+
+// let number = 13; // number
+// let isStudent = false; // boolean
+
+// let value; // undefined
+
+// let emptyValue = null; // null
+
+// console.log(text);
+
+// console.log(number);
+
+// console.log(isStudent);
+// console.log(value);
+
+// console.log(emptyValue);
+
+// let num1 = 20;
+// let num2 = 20;
+
+// let sum = num1 + num2;
+
+// console.log("total value ", sum);
+
+// const user = {
+//   name: "arnob",
+//   age: 18,
+//   country: "bangladesh",
+// };
+
+// console.log(user.name);
+// console.log(user.age);
+
+// console.log(user.country);
+
+// let tep = 32;
+
+// if (tep < 32) {
+//   console.log("hot");
+// } else {
+//   console.log("cold");
+// }
+
+// var  fuction scope
+// let block scope
+// const block scope
+
+// let userName = prompt("Enter your name");
+
+// console.log(userName);
+
+// function greetUser(name) {
+//   console.log("Hello " + name);
+// }
+
+// let userName = "Amir";
+
+// userName;
+
+// greetUser(userName);
